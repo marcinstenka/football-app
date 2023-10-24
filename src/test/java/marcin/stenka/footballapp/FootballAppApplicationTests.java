3977298b817ee2b65002ce624fc83df3aae8544b
Marcin Stenka <mastenka@wp.pl>
2023-10-24 16:22:30 +0200

    Initial commit

---
+++ src/test/java/marcin/stenka/footballapp/FootballAppApplicationTests.java
@@ -0,0 +1,13 @@
+package marcin.stenka.footballapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
