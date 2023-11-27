1dfcc2618feac77f30f548028c7d229a78549717
marcinstenka <mastenka@wp.pl>
2023-11-27 08:59:21 +0100

    main project files reorganized

---
+++ football-app/src/main/java/marcin/stenka/footballapp/club/service/ClubService.java
@@ -0,0 +1,18 @@
+package marcin.stenka.footballapp.club.service;
+
+import marcin.stenka.footballapp.club.entity.Club;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ClubService {
+    List<Club> findAll();
+    Club findByName(String name);
+    Optional<Club> findById(UUID id);
+    void addClub(Club club);
+    void updateClub(Club club);
+    void deleteAll();
+    void deleteById(UUID id);
+
+}
